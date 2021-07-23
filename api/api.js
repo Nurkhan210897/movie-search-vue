c15f4c3ac2211bccfc87cedba5fccec9f997396c
Nurhanfront <front@studionomad.kz>
2021-07-23 17:17:12 +0600

    deleted: api js

--- api/api.js
+++
@@ -1,8 +0,0 @@
-const KEY = '4ed03e49'
-
-export function loadMovie() {
-    fetch(`http://www.omdbapi.com/?s=marvel&apikey=${KEY}&page=1`)
-        .then((e) => {
-            console.log(e);
-        })
-}
